c3465a9004edb4964cc2b904f15bb5b0257e04c7
Riokrid <121547293+rinooktavianridwan@users.noreply.github.com>
2023-07-14 18:09:02 +0700

    Add files via upload

---
+++ SMT1/SMT1/Nama.java
@@ -0,0 +1,12 @@
+
+package SMT1;
+
+public class Nama {
+    public static void main(String[] args) {
+        System.out.println("Nama    : Rino Oktavian Ridwan");
+        System.out.println("Berkas  : Kuliah");
+        System.out.println("Tipe    : SMT 1");
+        System.out.println("Bahasa  : Java");
+        
+    }
+}
